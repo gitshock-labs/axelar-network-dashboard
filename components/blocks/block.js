b4bcb5026d7004ddfb0aecf92613c658ed7f4a58
Sirapop Na Ranong <sirapop@Sirapops-MacBook-Air.local>
2021-09-13 03:00:04 +0700

    get txs from cosmos

--- components/blocks/block.js
+++ components/blocks/block.js
@@ -1,12 +1,14 @@
 import { useState, useEffect } from 'react'
 
 import moment from 'moment'
+import _ from 'lodash'
 
 import BlockDetail from './block-detail'
 import TransactionsTable from '../transactions/transactions-table'
 import Widget from '../widget'
 
-import { getBlock, getTransactions } from '../../lib/api/query'
+import { getBlock } from '../../lib/api/query'
+import { transactions as getTransactions } from '../../lib/api/cosmos'
 import { numberFormat } from '../../lib/utils'
 
 export default function Block({ height }) {
@@ -21,11 +23,21 @@ export default function Block({ height }) {
         setBlock({ data: response.data || {}, height })
       }
 
-      response = await getTransactions({ height })
+      // response = await getTransactions({ query: { match: { height } }, size: 100, sort: [{ timestamp: 'desc' }] })
 
-      if (response) {
-        setTransactions({ data: response.data || [], height })
+      let data = []
+
+      let pageKey = true
+
+      while (pageKey) {
+        response = await getTransactions({ events: `tx.height=${height}`, 'pagination.key': pageKey && typeof pageKey === 'string' ? pageKey : undefined })
+
+        data = _.orderBy(_.uniqBy(_.concat(data, (response && response.data) || []), 'txhash'), ['timestamp'], ['desc'])
+
+        pageKey = response && response.pagination && response.pagination.next_key
       }
+
+      setTransactions({ data, total: response && response.total, height })
     }
 
     if (height) {
@@ -43,7 +55,7 @@ export default function Block({ height }) {
         title={<div className="flex items-center text-gray-900 dark:text-white text-lg font-semibold space-x-1 mt-3">
           <span>Transactions</span>
           {transactions && transactions.height === height && (
-            <span>({transactions.data.length})</span>
+            <span>({transactions.total || transactions.data.length})</span>
           )}
         </div>}
         className="mt-4"
