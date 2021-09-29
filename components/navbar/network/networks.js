4a164137da13ee7f2c51d0ebabd22f5cf7bc47d4
Sirapop Na Ranong <sirapop@Sirapops-MacBook-Air.local>
2021-09-29 11:50:35 +0700

    disable mainnet menu

--- components/navbar/network/networks.js
+++ components/navbar/network/networks.js
@@ -6,15 +6,25 @@ export default function Networks({ handleDropdownClick }) {
       <div className="dropdown-title">Change Network</div>
       <div className="flex flex-wrap pb-1">
         {networks.map((item, i) => (
-          <a
-            key={i}
-            href={item.url}
-            onClick={handleDropdownClick}
-            className="dropdown-item w-1/2 flex items-center justify-start space-x-1.5 p-2"
-          >
-            {item.icon}
-            <span className="text-xs">{item.title}</span>
-          </a>
+          item.disabled ?
+            <div
+              key={i}
+              title="Not available yet"
+              className="dropdown-item w-1/2 cursor-not-allowed flex items-center justify-start space-x-1.5 p-2"
+            >
+              {item.icon}
+              <span className="text-xs">{item.title}</span>
+            </div>
+            :
+            <a
+              key={i}
+              href={item.url}
+              onClick={handleDropdownClick}
+              className="dropdown-item w-1/2 flex items-center justify-start space-x-1.5 p-2"
+            >
+              {item.icon}
+              <span className="text-xs">{item.title}</span>
+            </a>
         ))}
       </div>
     </>
