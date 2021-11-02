8cea5bf62c08bdece63b8217884db02ae2a6376f
Sirapop Na Ranong <sirapop@Sirapops-MacBook-Air.local>
2021-11-03 00:18:05 +0700

    add axelar-core version

--- components/slashing-info/index.js
+++ components/slashing-info/index.js
@@ -1,6 +1,8 @@
 import { useEffect } from 'react'
 import { useSelector, useDispatch, shallowEqual } from 'react-redux'
 
+import _ from 'lodash'
+
 import { slashingParams } from '../../lib/api/cosmos'
 import { numberFormat } from '../../lib/utils'
 
@@ -25,6 +27,17 @@ export default function SlashingInfo() {
         }
       }
 
+      if (!controller.signal.aborted) {
+        const res = await fetch(process.env.NEXT_PUBLIC_NETWORK_RELEASES_URL)
+        response = await res.text()
+
+        if (response?.includes('`axelar-core` version')) {
+          response = response.split('\n').filter(line => line?.includes('`axelar-core` version'))
+
+          slashingData = { ...slashingData, ...Object.fromEntries([_.head(response).split('|').map(s => s?.trim().split('`').join('').split(' ').join('_'))]) }
+        }
+      }
+
       if (slashingData) {
         dispatch({
           type: SLASHING_DATA,
@@ -44,9 +57,9 @@ export default function SlashingInfo() {
 
   return (
     <div className="w-full bg-gray-100 dark:bg-gray-900 rounded grid grid-flow-row grid-cols-2 sm:flex items-start text-2xs lg:text-xs gap-3 sm:gap-4 lg:gap-6 mt-0 sm:mt-auto mb-4 sm:mb-auto ml-0 sm:ml-2 py-3 px-4">
-      <div className="flex flex-col space-y-1 lg:space-y-0.5">
+      <div className="flex flex-col text-center space-y-1 lg:space-y-0.5">
         <span className="text-gray-700 dark:text-gray-500 font-semibold">Signed Blocks Window</span>
-        <span className="text-gray-900 dark:text-gray-300 font-light">
+        <span className="text-gray-900 dark:text-gray-300 text-xs font-light">
           {slashing_data ?
             slashing_data.signed_blocks_window ?
               <>{numberFormat(slashing_data.signed_blocks_window, '0,0.00000000')}</>
@@ -57,9 +70,9 @@ export default function SlashingInfo() {
           }
         </span>
       </div>
-      <div className="flex flex-col space-y-1 lg:space-y-0.5">
+      <div className="flex flex-col text-center space-y-1 lg:space-y-0.5">
         <span className="text-gray-700 dark:text-gray-500 font-semibold">Min Signed / Window</span>
-        <span className="text-gray-900 dark:text-gray-300 font-light">
+        <span className="text-gray-900 dark:text-gray-300 text-xs font-light">
           {slashing_data ?
             slashing_data.min_signed_per_window ?
               <>{numberFormat(slashing_data.min_signed_per_window, '0,0.00000000')}</>
@@ -70,9 +83,9 @@ export default function SlashingInfo() {
           }
         </span>
       </div>
-      <div className="flex flex-col space-y-1 lg:space-y-0.5">
+      <div className="flex flex-col text-center space-y-1 lg:space-y-0.5">
         <span className="text-gray-700 dark:text-gray-500 font-semibold">Slash Fraction Double Sign</span>
-        <span className="text-gray-900 dark:text-gray-300 font-light">
+        <span className="text-gray-900 dark:text-gray-300 text-xs font-light">
           {slashing_data ?
             slashing_data.slash_fraction_double_sign ?
               <>{numberFormat(slashing_data.slash_fraction_double_sign, '0,0.00000000')}</>
@@ -83,9 +96,9 @@ export default function SlashingInfo() {
           }
         </span>
       </div>
-      <div className="flex flex-col space-y-1 lg:space-y-0.5">
+      <div className="flex flex-col text-center space-y-1 lg:space-y-0.5">
         <span className="text-gray-700 dark:text-gray-500 font-semibold">Slash Fraction Downtime</span>
-        <span className="text-gray-900 dark:text-gray-300 font-light">
+        <span className="text-gray-900 dark:text-gray-300 text-xs font-light">
           {slashing_data ?
             slashing_data.slash_fraction_downtime ?
               <>{numberFormat(slashing_data.slash_fraction_downtime, '0,0.00000000')}</>
@@ -96,12 +109,25 @@ export default function SlashingInfo() {
           }
         </span>
       </div>
-      <div className="flex flex-col space-y-1 lg:space-y-0.5">
+      <div className="flex flex-col text-center space-y-1 lg:space-y-0.5">
         <span className="text-gray-700 dark:text-gray-500 font-semibold">Downtime Jail Duration</span>
-        <span className="text-gray-900 dark:text-gray-300 font-light">
+        <span className="text-gray-900 dark:text-gray-300 text-xs font-light">
           {slashing_data ?
             slashing_data.downtime_jail_duration ?
               <>{slashing_data.downtime_jail_duration}</>
+              :
+              '-'
+            :
+            <div className="skeleton w-8 h-4" />
+          }
+        </span>
+      </div>
+      <div className="flex flex-col text-center space-y-1 lg:space-y-0.5">
+        <span className="text-gray-700 dark:text-gray-500 font-semibold">Axelar Core Version</span>
+        <span className="text-gray-900 dark:text-gray-300 font-light">
+          {slashing_data ?
+            slashing_data[`axelar-core_version`] ?
+              <>{slashing_data[`axelar-core_version`]}</>
               :
               '-'
             :
