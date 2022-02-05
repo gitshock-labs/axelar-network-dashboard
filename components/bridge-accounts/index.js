8237b3f4b90d87d6877bd6f3e0c3d529586e047f
Sirapop <sirapop@Sirapops-MacBook-Air.local>
2022-02-05 16:20:18 +0700

    remove log

--- components/bridge-accounts/index.js
+++ components/bridge-accounts/index.js
@@ -102,9 +102,7 @@ export default function BridgeAccounts() {
             method: 'wallet_addEthereumChain',
             params: chains_data?.find(c => c.chain_id === chain_id)?.provider_params,
           })
-        } catch (error) {
-          console.log(error)
-        }
+        } catch (error) {}
       }
     }
 
